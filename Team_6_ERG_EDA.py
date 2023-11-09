ac557c54dfb733fdd2edcbfa08275711f71372f1
Grumily <113652315+Grumily@users.noreply.github.com>
2023-11-09 08:23:48 -0500

    Preliminary EDA by ERG
    Just a start... We probably want to rename variables! But the pre- / post-pandemic question make sense to me. There also seems to be a seasonal variation in rates of collision as Sairam noted-- that might be harder to explore (in past have done this using month as a factor, but functional fitting would make more sense).

---
+++ Team_6_ERG_EDA.py
@@ -0,0 +1,13 @@
+#%% [markdown]
+
+# Team 6 Project Code
+
+import pandas as pd
+import rfit 
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn import linear_model
+# %%
+dfacc = pd.read_csv('acc.csv')
+# %%
